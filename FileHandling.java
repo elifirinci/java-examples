846a3816c6e7479e39cb39c9bcf023906be62eca
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-02-05 15:31:07 +0300

    Create FileHandling.java

---
+++ FileHandling.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+public class MidtermDeneme3 {
+    public static void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        try{
+            File file=new File("file.txt");
+            BufferedWriter buffer=new BufferedWriter(new FileWriter(file,true));
+            String text=scanner.nextLine();
+            buffer.write(text);
+            Scanner reader=new Scanner(file);
+            while(reader.hasNextLine()){
+                String data=reader.nextLine();
+                System.out.println(data);
+            }
+
+            buffer.close();
+            scanner.close();
+            
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+    
+}

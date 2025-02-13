70bdbba9c747ddb69e829281730eca5e85e70840
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-02-13 12:56:37 +0300

    Create RabbitVsBirdsGrowth.java

---
+++ RabbitVsBirdsGrowth.java
@@ -0,0 +1,18 @@
+public class EightQuestion {
+    public static void main(String[] args){
+        int numbersOfRabbits = 1042;
+        int numbersOfBirds = 2272;
+        double rabbitGrowthRate = 0.038; 
+        double birdGrowthRate = 0.012; 
+        int years = 0;
+
+        while (numbersOfRabbits <= numbersOfBirds) {
+            numbersOfRabbits += (int) (numbersOfRabbits * rabbitGrowthRate);
+            numbersOfBirds += (int) (numbersOfBirds * birdGrowthRate);
+            years++;
+        }
+
+        System.out.println("It will take " + years + " years for the number of rabbits to exceed the number of birds.");
+
+    }
+}

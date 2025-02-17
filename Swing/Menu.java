319d2691aa14970f68b82a4f5db3b2ec57cd395c
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-02-17 19:02:57 +0300

    Update Menu.java

--- Swing/Menu.java
+++ Swing/Menu.java
@@ -1,10 +1,10 @@
 import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+
 public class Menu{
     public static void main(String[] args){
         JFrame f=new JFrame();

32be16530b060b609a3a77876d3ae86d31ff15cb
Elif FIRINCI <100481883+elifirinci@users.noreply.github.com>
2025-03-10 18:10:24 +0300

    Create CalculatingCircleArea.java

---
+++ CalculatingCircleArea.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+	static Scanner scanner=new Scanner(System.in);
+	public static void calculate_field_circle(int number){
+			double pi=3.14;
+			double field=pi*Math.pow(number,2);
+			System.out.println("Field is " + field);
+			
+			
+			
+		}
+	public static void main(String[] args) {
+		System.out.println("Enter a radius: ");
+		int number=scanner.nextInt();
+		calculate_field_circle(number);	
+	}
+}
